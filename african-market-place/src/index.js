1cd1cdb25213e3a2d894b5465df1e090aba5e3e0
pak11273 <pak11273@gmail.com>
2021-01-31 16:21:47 -0600

    fixes all of the console warnings

--- african-market-place/src/index.js
+++ african-market-place/src/index.js
@@ -1,15 +1,10 @@
 import './assets/css/main.css';
 
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  useHistory,
-} from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 import { ExampleListPage } from './components/pages/ExampleList';
 import { LandingPage } from './components/pages/Landing';
-import { LoadingComponent } from './components/common';
+// import { LoadingComponent } from './components/common';
 import { NotFoundPage } from './components/pages/NotFound';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -26,11 +21,10 @@ ReactDOM.render(
 function App() {
   // The reason to declare App this way is so that we can use any helper functions we'd need for business logic, in our case auth.
   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
-  const history = useHistory();
+  // const history = useHistory();
 
   return (
     <Switch>
-
       <Route path="/landing" component={LandingPage} />
       {/* any of the routes you need secured should be registered as SecureRoutes */}
       <Route path="/" exact component={() => <LandingPage />} />
