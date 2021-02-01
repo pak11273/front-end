e0d4b1b74456d0f96e21bbf9cf965d50ae62ac7a
pak11273 <pak11273@gmail.com>
2021-01-31 21:48:22 -0600

    finished basic ui for login and signup page

--- african-market-place/src/App.js
+++ african-market-place/src/App.js
@@ -1,9 +1,9 @@
+import React, { useState } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { LandingPage } from './components/pages/Landing';
 import { LoginPage } from './components/pages/Login';
 import { NotFoundPage } from './components/pages/NotFound';
-import React from 'react';
 import { SignUpPage } from './components/pages/SignUp';
 
 export default function App() {
@@ -11,12 +11,34 @@ export default function App() {
   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
   // const history = useHistory();
 
+  const initialForm = {
+    username: '',
+    email: '',
+    password: '',
+    confirm: '',
+  };
+
+  const [form, setForm] = useState(initialForm);
+
+  const onChange = e => {
+    //pending
+    const [name, value] = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
   return (
     <Switch>
       {/* any of the routes you need secured should be registered as SecureRoutes */}
       <Route path="/" exact component={() => <LandingPage />} />
-      <Route path="/signup" component={SignUpPage} />
-      <Route path="/login" component={LoginPage} />
+      <Route path="/signup">
+        <SignUpPage onChange={onChange} {...form} />
+      </Route>
+      <Route path="/login">
+        <LoginPage onChange={onChange} {...form} />
+      </Route>
 
       <Route component={NotFoundPage} />
     </Switch>
