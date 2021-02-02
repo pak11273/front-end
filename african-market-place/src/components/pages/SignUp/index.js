e961a6ce877c4595e09d6b3cdfcac8796e917aa0
Joe Park <parkjs3@yahoo.com>
2021-02-01 17:09:39 -0800

    finished form validations

--- african-market-place/src/components/pages/SignUp/index.js
+++ african-market-place/src/components/pages/SignUp/index.js
@@ -2,66 +2,75 @@ import * as yup from "yup";
 
 import React, { useState } from 'react';
 
+import axios from "axios";
 import schema from "../../../schema";
 
 export const SignUpPage = () => {
-  const initialForm = {
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-    country: "",
-  };
-
-  const [form, setForm] = useState(initialForm);
-
-  const onChange = e => {
-    const { name, value } = e.target;
-    updateSignInForm(name, value);
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
-
-  const onSubmit = e => {
-    e.preventDefault();
-    // redirect to home page on successful validation
-  };
 
   const errors = {
     username: "",
     email: "",
     country: "",
     password: "",
-    confirmPassword: "",
+    first_name: "",
+    last_name: "",
+    user_role: "",
   };
 
   const formValues = {
     username: '',
     email: '',
     password: '',
-    confirmPassword: '',
     country: "",
+    first_name: "",
+    last_name: "",
+    user_role: false, 
   };
 
+  const [disabled, setDisabled] = useState(true);
   const [signUpErrors, setSignUpErrors] = useState(errors);
   const [signUpFormValues, setSignUpFormValues] = useState(formValues);
 
+  const onChange = e => {
+    const { name, value, checked } = e.target;
+
+    let valueOf = "";   
+
+    if(checked) {
+      valueOf = !signUpFormValues.user_role.value;
+    } else {
+        valueOf = value;
+    }
+      updateSignInForm(name, valueOf);
+    };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    console.log('signup: ', signUpFormValues);
+    axios.post("api" ,signUpFormValues)
+        .then((response)=>{
+          console.log(response);
+        })
+        .catch((error)=>{
+          console.log(error);
+        });
+    setSignUpFormValues(formValues);
+  };
+
   const updateSignInForm = (name, value) => {
-    console.log('name: ', name);
-    console.log('value: ', value);
     yup
       .reach(schema, name)
       .validate(value)
       .then(() => {
         setSignUpErrors({ ...signUpErrors, [name]: "" });
+        setDisabled(false)
       })
       .catch((error) => {
         setSignUpErrors({ ...signUpErrors, [name]: error.errors[0] });
       });
 
-    // setSignUpFormValues({ ...signUpFormValues, [name]: value });
+      setSignUpFormValues({ ...signUpFormValues, [name]: value });
   };
 
   return (
@@ -81,10 +90,41 @@ export const SignUpPage = () => {
                 <input
                   type="text"
                   name="username"
-                  value={form.username}
+                  value={signUpFormValues.username}
                   onChange={onChange}
                 />
               </label>
+              <div style={{color: "red"}}>{signUpErrors.username}</div> 
+              <label
+                style={{
+                  maxWidth: '200px',
+                  margin: '40px auto',
+                }}
+              >
+                first name
+                <input
+                  type="text"
+                  name="first_name"
+                  value={signUpFormValues.first_name}
+                  onChange={onChange}
+                />
+              </label>
+              <div style={{color: "red"}}>{signUpErrors.first_name}</div> 
+              <label
+                style={{
+                  maxWidth: '200px',
+                  margin: '40px auto',
+                }}
+              >
+                last name
+                <input
+                  type="text"
+                  name="last_name"
+                  value={signUpFormValues.last_name}
+                  onChange={onChange}
+                />
+              </label>
+              <div style={{color: "red"}}>{signUpErrors.last_name}</div>
               <label
                 style={{
                   maxWidth: '200px',
@@ -95,10 +135,11 @@ export const SignUpPage = () => {
                 <input
                   type="email"
                   name="email"
-                  value={form.email}
+                  value={signUpFormValues.email}
                   onChange={onChange}
                 />
               </label>
+              <div style={{color: "red"}}>{signUpErrors.email}</div> 
               <label
               style={{
                   maxWidth: '200px',
@@ -109,10 +150,11 @@ export const SignUpPage = () => {
                 <input 
                   type="text"
                   name="country"
-                  value={form.country}
+                  value={signUpFormValues.country}
                   onChange={onChange}
                 />    
               </label>
+              <div style={{color: "red"}}>{signUpErrors.country}</div> 
               <label
                 style={{
                   maxWidth: '200px',
@@ -123,27 +165,30 @@ export const SignUpPage = () => {
                 <input
                   type="password"
                   name="password"
-                  value={form.password}
+                  value={signUpFormValues.password}
                   onChange={onChange}
                 />
               </label>
-              <label
-                style={{
+              <div style={{color: "red"}}>{signUpErrors.password}</div> 
+              <label style={{
                   maxWidth: '200px',
                   margin: '40px auto',
                 }}
-              >
-                confirm password
-                <input
-                  type="password"
-                  name="confirmPassword"
-                  value={form.confirmPassword}
-                  onChange={onChange}
+              > check box if owner
+                <input style={{
+                  opacity: 1,
+                  height: "20px",
+                  width: "20px",
+                  border: "1px solid white",
+                  appearance: "auto"
+                }}
+                type="checkbox" name="user_role" value={signUpFormValues.user_role}
+                onChange={onChange}
                 />
               </label>
               <ul className="actions special">
                 <li>
-                  <button className="button primary">Sign Up</button>
+                  <button className="button primary" disabled={disabled}>Sign Up</button>
                 </li>
               </ul>
             </div>
--- african-market-place/src/schema.js
+++ african-market-place/src/schema.js
@@ -1,13 +1,14 @@
 import * as yup from "yup";
 
 let schema = yup.object().shape({
-    username: yup.string().min(6).max(40).required(),
-    firstName: yup.string().max(30).required(),
-    lastName: yup.string().max(30).required(),
-    email: yup.string().max(40).required(),
-    country: yup.string().max(30).required(),
-    password: yup.string().max(40).required(),
-    confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
+    username: yup.string().min(6).max(40).required("username is required"),
+    firstName: yup.string().max(30).required("first name is required"),
+    lastName: yup.string().max(30).required("last name is required"),
+    email: yup.string().max(40).required("email is required"),
+    country: yup.string().max(30).required("country is required"),
+    password: yup.string().max(40).required("password is required"),
+    // confirmPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match'),
+    owner: yup.boolean()
 });
 
 export default schema;
