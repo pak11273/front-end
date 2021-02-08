9801acb332a8b572e9ab93467d884110833e9061
pak11273 <pak11273@gmail.com>
2021-02-07 22:52:07 -0600

    style 404 page

--- african-market-place/src/components/pages/NotFound/index.js
+++ african-market-place/src/components/pages/NotFound/index.js
@@ -3,19 +3,25 @@ import React from 'react';
 
 export const NotFoundPage = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexFlow: 'column wrap',
-        alignItems: 'center',
-        height: '100vh',
-        marginTop: '100px',
-      }}
-    >
-      <h1>404 Page Not Found</h1>
-      <Link to="/">
-        <button>Back To Home</button>
-      </Link>
+    <div id="page-wrapper">
+      <div className="landing is-preload" id="login">
+        <section id="banner">
+          <div className="inner">
+            <div
+              style={{
+                display: 'flex',
+                flexFlow: 'column wrap',
+                alignItems: 'center',
+              }}
+            >
+              <h1>404 Page Not Found</h1>
+              <Link to="/">
+                <button>Back To Home</button>
+              </Link>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
